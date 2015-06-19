597ee036177732d002082fe5c24d0237c19873c3
GreenKhulta <elhmidi.hicham@gmail.com>
2015-06-19 18:55:59 +0200

    refactoring

--- server/es5/api.js
+++ server/es5/api.js
@@ -5,13 +5,12 @@ var serveStatic = require('serve-static');
 var moment = require('moment');
 var path = require('path');
 
-var db = require('./db');
-
 var app = express();
 
 var CronJob = require('cron').CronJob;
 var controller = require('./controller');
 
+//execute every hour
 var job = new CronJob('00 * * * *', controller.searchTweets, function () {
   console.log('cron executed at %s', new Date());
 }, true /* Start the job right now */
@@ -19,31 +18,20 @@ var job = new CronJob('00 * * * *', controller.searchTweets, function () {
 
 app.use(express['static'](path.join(__dirname, '../../client')));
 
+//serve static html
 app.get('/', function (req, res) {
   res.sendFile('index.html', { root: path.join(__dirname, '../../client') });
 });
 
-app.get('/searc', function (req, res) {
-  controller.searchTweets();
-});
+app.get('/search', controller.searchTweets);
 
-app.get('/videos', function (req, res) {
-  console.log('getting videos');
-
-  db.connect().then(function (db) {
-    var collection = db.collection('post');
-    collection.find({}).toArray(function (err, data) {
-      if (err) return err;
-      res.json(data);
-    });
-  });
-});
+app.get('/videos', controller.findFromDB);
 
 var server = app.listen(3000, function () {
 
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('app listening at http://%s:%s', host, port);
 });
 //# sourceMappingURL=api.js.map
--- server/es6/api.js
+++ server/es6/api.js
@@ -3,13 +3,14 @@ var serveStatic = require('serve-static');
 var moment = require("moment");
 var path = require("path");
 
-var db = require("./db");
+
 
 var app = express();
 
 var CronJob = require('cron').CronJob;
 var controller = require("./controller");
 
+//execute every hour
 var job = new CronJob('00 * * * *', controller.searchTweets, () => {
     console.log("cron executed at %s", new Date());
   },
@@ -18,32 +19,20 @@ var job = new CronJob('00 * * * *', controller.searchTweets, () => {
 
 app.use(express.static(path.join(__dirname, '../../client')));
 
+//serve static html
 app.get('/',  (req, res) => {
   	res.sendFile("index.html", { root: path.join(__dirname, '../../client') });
 });
 
-app.get('/searc',  (req, res) => {
-  	controller.searchTweets();
-});
+app.get('/search', controller.searchTweets);
 
-app.get('/videos',  (req, res) => {
-    console.log("getting videos");
-
-    db.connect().then( (db) => {
-      var collection = db.collection("post");
-      collection.find({}).toArray(function (err, data) {
-        if (err) 
-          return err;
-        res.json(data);
-      });
-    });
-});
+app.get('/videos',  controller.findFromDB);
 
 var server = app.listen(3000, () => {
 
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('app listening at http://%s:%s', host, port);
 
 });
