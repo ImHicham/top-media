26f8c3139a10594dfd918074c2292f1923a155ac
GreenKhulta <elhmidi.hicham@gmail.com>
2015-06-19 19:00:08 +0200

    implemented method findFromDB

--- server/es6/controller.js
+++ server/es6/controller.js
@@ -1,8 +1,47 @@
 var YoutubeSource = require("./model/youtubeSource");
+var db = require("./db");
+
+
+
 
 module.exports = {
-	searchTweets () {
+	searchTweets (req, res) {
 		let instance = new YoutubeSource();
-  		instance.search().then( tweets => instance.filter(tweets) ).then( tweets => instance.transform(tweets) ).then( tweets => instance.save(tweets) );
+  		instance.search()
+  						.then( tweets => instance.filter(tweets) )
+  						.then( tweets => instance.transform(tweets) )
+  						.then( tweets => instance.save(tweets) )
+  						.then( () => res.sendStatus(200) );
+	},
+
+	findFromDB (req, res) {
+		console.log("getting videos");
+
+	    let options = req.query;
+	    let searchCriteria = {};
+
+	    //if contains 'best'
+	    if ( /best/.test(options.sort) ) {
+	      options.sort.push(['retweet_count','desc'], ['favorite_count','desc']);
+	    }
+
+	    //if contains 'newest'
+	    if ( /newest/.test(options.sort) ) {
+	    	options.sort.push(['scraped_at','desc']);
+	    };
+
+	    if (options.from && options.to) {
+	    	searchCriteria.scraped_at = {$gte :  options.from, $lte : options.to};
+	    };
+
+	    db.connect().then( (db) => {
+	      var collection = db.collection("post");
+	      collection.find(searchCriteria, options).toArray(function (err, data) {
+	        if (err) 
+	          return err;
+	        res.json(data);
+	      });
+	    });
 	}
+
 }
