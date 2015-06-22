018db2233cd279296984fc54ab50acc9ac2da058
GreenKhulta <elhmidi.hicham@gmail.com>
2015-06-22 02:30:19 +0200

    'findFromDB' fixed. now sorting correctly

--- server/es6/controller.js
+++ server/es6/controller.js
@@ -8,35 +8,40 @@ module.exports = {
 	searchTweets (req, res) {
 		let instance = new YoutubeSource();
   		instance.search()
-  						.then( tweets => instance.filter(tweets) )
-  						.then( tweets => instance.transform(tweets) )
-  						.then( tweets => instance.save(tweets) )
-  						.then( () => res.sendStatus(200) );
+  			.then( tweets => instance.filter(tweets) )
+  			.then( tweets => instance.transform(tweets) )
+  			.then( tweets => instance.save(tweets) )
+  			.then( () => res.sendStatus(200) );
 	},
 
 	findFromDB (req, res) {
 		console.log("getting videos");
 
-	    let options = req.query;
+	    let params = req.query;
 	    let searchCriteria = {};
-
+	    let projection = {};
+	    let options = {
+	    	"skip": params.skip,
+	    	"limit": params.limit,
+	    	"sort": []
+	    };
 	    //if contains 'best'
-	    if ( /best/.test(options.sort) ) {
-	      options.sort.push(['retweet_count','desc'], ['favorite_count','desc']);
+	    if ( /best/.test(params.sort) ) {
+	      options.sort.push(["retweet_count", "desc"]);
+	      options.sort.push(["favorite_count", "desc"]);
 	    }
-
 	    //if contains 'newest'
-	    if ( /newest/.test(options.sort) ) {
-	    	options.sort.push(['scraped_at','desc']);
+	    if ( /newest/.test(params.sort) ) {
+	    	options.sort.push(["scraped_at", "desc"]);
 	    };
 
-	    if (options.from && options.to) {
-	    	searchCriteria.scraped_at = {$gte :  options.from, $lte : options.to};
+	    if (params.from && params.to) {
+	    	searchCriteria.scraped_at = {$gte :  params.from, $lte : params.to};
 	    };
 
 	    db.connect().then( db => {
 	      var collection = db.collection("post");
-	      collection.find(searchCriteria, options).toArray(function (err, data) {
+	      collection.find(searchCriteria, projection, options).toArray(function (err, data) {
 	        if (err) 
 	          return err;
 	        res.json(data);
